1b8d25504abff75fda9912bc312abf882a8e04d9
hangyudu <hangyudu@tencent.com>
2024-11-28 20:52:31 +0800

    [main][add]first version

--- transaction/two-phase-commit/src/main.rs
+++ transaction/two-phase-commit/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::future::Future;
+
+mod transaction;
+
+// Boilerplate which lets us write `async fn main`, we'll explain it later.
+#[tokio::main]
+async fn main() {
+    let (sender, receiver) = async_channel::unbounded();
+    let coordinator = &mut transaction::Coordinator::new(10, &sender, &receiver);
+    coordinator.transaction();
+
+    while !coordinator.transaction_end() {
+        for x in &coordinator.participants {
+        }
+    }
 }
